1fb71e1ce965e9cef207f5e9db864bcaf7a7117b
kinshuk122 <kinshuk364@gmail.com>
2024-03-27 13:46:20 +0100

    Trial Gravity cube

---
+++ WarDogs/Assets/Scripts/DirectionalGravity.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalGravity : MonoBehaviour
+{
+    [SerializeField] private Transform testCube;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            testCube.eulerAngles = new Vector3(90f, 0f, 0f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            testCube.eulerAngles = new Vector3(180f, 0f, 0f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            testCube.eulerAngles = new Vector3(270f, 0f, 0f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            testCube.eulerAngles = new Vector3(360f, 0f, 0f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            testCube.eulerAngles = new Vector3(0f, 0f, -90f);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            testCube.eulerAngles = new Vector3(0f, 0f, 90f);
+        }
+    }
+}
